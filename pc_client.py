4aa3a26bff4ab802d446680b2e176076b92667ea
readease1 <ayyubhearthstone1@outlook.com>
2025-06-09 01:36:26 +0100

    Update pc_client.py

--- pc_client.py
+++ pc_client.py
@@ -12,7 +12,7 @@ from pynput.keyboard import Key
 import sys
 
 # Your Heroku app URL - UPDATE THIS WITH YOUR ACTUAL HEROKU APP NAME!
-SERVER_URL = 'https://cow714-osrs-controller.herokuapp.com'
+SERVER_URL = 'https://cow714-osrs-controller-221dee6aaaf8.herokuapp.com/'
 
 # Game window coordinates - UPDATED WITH YOUR ACTUAL OSRS WINDOW!
 GAME_WINDOW = {
